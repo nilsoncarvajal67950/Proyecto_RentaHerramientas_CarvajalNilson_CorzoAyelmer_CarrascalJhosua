fab7cb2f62d3a2d2a2ed06829592ae720e3b3b88
AyelmerCorzoB <corzobarbosaa@gmail.com>
2025-05-25 10:18:18 -0500

    feat: :sparkles: Mejora la función getReservations para incluir encabezados de usuario y rol en la solicitud API

--- js/components/Reservations/ReservationService.js
+++ js/components/Reservations/ReservationService.js
@@ -4,8 +4,18 @@ class ReservationService {
   }
 
   async getReservations() {
-    return this.apiService.request('api/alkile/reservations');
-  }
+    const role = localStorage.getItem("userRole");
+    const userId = localStorage.getItem("userId");
+
+    return this.apiService.request('/alkile/reservations', {
+        method: "GET",
+        headers: {
+            "userId": userId,
+            "role": role
+        }
+    });
+}
+
 
   async createReservation(reservationData) {
     return this.apiService.request('api/alkile/reservations', {
